0f1c56ec7b37ac2bce4c1af50445f5aa3786659c
Edd Williams <edd.williams@me.com>
2017-01-12 23:24:04 +0000

    Moved to correct place

---
+++ html/targetup.php
@@ -0,0 +1,22 @@
+<?php 
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '**Password In Here**';
+	$dbname = 'temperature';
+	
+	$conn = new mysqli($dbhost,$dbuser,$dbpass, $dbname);
+	
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql = "UPDATE temp SET target=target + 1";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Record updated successfully";
+	} else {
+		echo "Error updating record: " . $conn->error;
+	}
+	$conn->close();
+	?>
